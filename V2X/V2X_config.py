6f1b8dc062628c3a1cadeb4b6eb1f2a1433232f2
Fernando Sevilla Martínez <54994431+SevillaFe@users.noreply.github.com>
2025-07-10 22:15:10 +0200

    Create V2X_config.py
    Initial Version

---
+++ V2X/V2X_config.py
@@ -0,0 +1,6 @@
+# config/config.py
+
+# V2X Communication Settings
+V2X_BROADCAST_IP = "255.255.255.255"  # Broadcast to all V2X-capable devices
+V2X_PORT = 5005                      # Standard port for V2X UDP communication
+BUFFER_SIZE = 1024                  # Bytes to read from socket
