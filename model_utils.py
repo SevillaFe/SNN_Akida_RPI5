41d8b2b27b03599fb5723633f682ebd3b05691f7
Fernando Sevilla Martínez <54994431+SevillaFe@users.noreply.github.com>
2025-07-10 22:24:11 +0200

    Delete model_utils.py
    Delete

--- model_utils.py
+++
@@ -1,8 +0,0 @@
-### optional, for future extension
-
-def serialize_weights(model):
-    return {"weights": model.get_weights().tolist()}
-
-def load_model():
-    # Placeholder function for loading a model
-    pass
