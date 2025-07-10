ec50f8d1b7c4a0872206b345670dcec001188ceb
Fernando Sevilla Martínez <54994431+SevillaFe@users.noreply.github.com>
2025-07-10 22:06:12 +0200

    Delete MQTT_akida_runn.sh
    No needed. Old

--- MQTT_akida_runn.sh
+++
@@ -1,6 +0,0 @@
-###  MQTT Broker on RPI5 to use it previously to RPI5_MQTT.py
-
-sudo apt update
-sudo apt install mosquitto mosquitto-clients -y
-sudo systemctl enable mosquitto
-sudo systemctl start mosquitto
