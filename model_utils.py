569a2bf3ccd90fdc8d645626bf09e0f6330bd73d
Fernando Sevilla Martínez <54994431+SevillaFe@users.noreply.github.com>
2025-07-10 21:33:23 +0200

    Create model_utils.py
    Initial Version

---
+++ model_utils.py
@@ -0,0 +1,8 @@
+### optional, for future extension
+
+def serialize_weights(model):
+    return {"weights": model.get_weights().tolist()}
+
+def load_model():
+    # Placeholder function for loading a model
+    pass
